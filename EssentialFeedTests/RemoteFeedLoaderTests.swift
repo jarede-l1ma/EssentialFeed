ece6464a8d45b3e1e754d6df4292f9de8c9ae52a
Járede Lima <jarede.lima@a.ucb.br>
2023-10-09 17:50:21 -0300

    `RemoteFeedLoader` does not request data upon creation

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -5,4 +5,21 @@
 //  Created by Jarede Lima on 09/10/23.
 //
 
-import Foundation
+import XCTest
+
+class RemoteFeedLoader {
+    
+}
+
+class HTTPClient {
+    var requestedURL: URL?
+}
+
+class RemoteFeedLoaderTests: XCTestCase {
+    func test_init() {
+        let client = HTTPClient()
+        _ = RemoteFeedLoader()
+        
+        XCTAssertNil(client.requestedURL)
+    }
+}
