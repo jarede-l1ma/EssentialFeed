c9a321b8d21b3d0f5d0561dacd7739e41c7486a6
Járede Lima <jarede.lima@a.ucb.br>
2023-10-09 19:49:47 -0300

    Add RemoteFeedLoader Result type success/failure cases

--- EssentialFeed/FeedItem.swift
+++ EssentialFeed/FeedItem.swift
@@ -7,9 +7,10 @@
 
 import Foundation
 
-struct FeedItem {
+public struct FeedItem: Equatable {
     let id: UUID
     let description: String?
     let location: String?
     let imageURL: URL
 }
+
--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -62,7 +62,7 @@ class RemoteFeedLoaderTests: XCTestCase {
             client.complete(withStatusCode: 200, data: invalidJSON)
         })
     }
-    
+
     //MARK: - Helpers
     
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
@@ -72,12 +72,15 @@ class RemoteFeedLoaderTests: XCTestCase {
     }
     
     private func expect(_ sut: RemoteFeedLoader, toCompleteWithError error: RemoteFeedLoader.Error, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
-        var capturedErrors = [RemoteFeedLoader.Error]()
-        sut.load { capturedErrors.append($0) }
+        var capturedResults = [RemoteFeedLoader.Result]()
+        sut.load { capturedResults.append($0) }
         
         action()
         
-        XCTAssertEqual(capturedErrors, [error], file: file, line: line)
+        XCTAssertEqual(capturedResults,
+                       [.failure(error)],
+                       file: file,
+                       line: line)
     }
     
     private class HTTPClientSpy: HTTPClient {
