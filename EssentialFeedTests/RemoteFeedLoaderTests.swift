a71180fed534d83d0aba7c14360d8c26b6af92a9
Járede Lima <jarede.lima@a.ucb.br>
2023-10-09 19:06:56 -0300

    Remove forgoted line

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -52,7 +52,6 @@ class RemoteFeedLoaderTests: XCTestCase {
         var capturedErrors = [RemoteFeedLoader.Error]()
         sut.load { capturedErrors.append($0) }
         
-        let clientError = NSError(domain: "Test", code: 0)
         client.complete(withStatusCode: 400)
         
         XCTAssertEqual(capturedErrors, [.invalidData])
